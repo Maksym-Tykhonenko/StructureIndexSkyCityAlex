9b21511df4e1533d34827764342faa56497c6748
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-01-19 20:04:59 +0200

    ccc

--- [StructureIndexSkyCity]/IndexCityNavigation/SkyCityStack.js
+++ [StructureIndexSkyCity]/IndexCityNavigation/SkyCityStack.js
@@ -340,7 +340,7 @@ const SkyCityStack = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-01-15T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-01-22T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
